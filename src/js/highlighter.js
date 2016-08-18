ea25acc3f83bc81e667bafc6a8a4b134e341c1a2
Khaled <elgendy1204@yahoo.com>
2016-08-18 13:16:51 +0200

    add es6

--- src/js/highlighter.js
+++ src/js/highlighter.js
@@ -3,92 +3,88 @@ import highlighterActivation from './structure/highlighteractivation.js';
 import drawStyles from './structure/drawstyles.js';
 import saveload from './structure/saveload.js';
 
-var Highlighter = (function($) {
-
-	function Highlighter(options) {
+class Highlighter {
+	constructor(options) {
 		this.options = options;
-		this.image = '';
+		this.imageData = '';
 		initiation.initCanvasOnElement(this);
 	}
 
-	Highlighter.prototype.getElement = function() {
+	getElement() {
 		let element = document.querySelector(`#${this.options.element}`);
 		if(!this.options.element || !element ){
 			throw 'element id not detected';
 		}
 		return element;
-	};
+	}
 
-	Highlighter.prototype.getCanvasElement = function() {
+	getCanvasElement() {
 		return this.options.canvasElement;
-	};
+	}
 
-	Highlighter.prototype.getCanvasContainer = function() {
+	getCanvasContainer() {
 		return this.options.canvasContainer;
-	};
+	}
 
-	Highlighter.prototype.getCanvasContainerDimensions = function() {
+	getCanvasContainerDimensions() {
 		return this.options.canvasContainerDimensions;
-	};
+	}
 
-	Highlighter.prototype.getWholeContainer = function() {
+	getWholeContainer() {
 		return this.options.container ? document.querySelector(this.options.container) : document.querySelector('body');
-	};
+	}
 
-	Highlighter.prototype.getZIndex = function() {
+	getZIndex() {
 		return this.options.zIndex || '1';
-	};
+	}
 
-	Highlighter.prototype.getCalibarationLevel = function() {
+	getCalibarationLevel() {
 		return this.options.calibarationLevel || 10;
-	};
+	}
 
-	Highlighter.prototype.getLanguage = function() {
+	getLanguage() {
 		return this.options.lang || 'e';
-	};
+	}
 
-	Highlighter.prototype.getRadius = function() {
+	getRadius() {
 		return this.options.radius || 10;
-	};
+	}
 
-	Highlighter.prototype.getContext = function() {
+	getContext() {
 		return this.getCanvasElement().getContext('2d');
-	};
+	}
 
-	Highlighter.prototype.getOpacity = function() {
+	getOpacity() {
 		return this.options.opacity;
-	};
+	}
 
-	Highlighter.prototype.getColor = function() {
+	getColor() {
 		return this.options.color || 'yellow';
-	};
+	}
 
-	Highlighter.prototype.activateHighlighter = function() {
+	activateHighlighter() {
 		highlighterActivation.activate(this);
-	};
+	}
 
-	Highlighter.prototype.deactivateHighlighter = function() {
+	deactivateHighlighter() {
 		highlighterActivation.deactivate(this);
-	};
+	}
 
-	Highlighter.prototype.startMarker = function() {
+	startMarker() {
 		drawStyles.initMarkStyle(this);
-	};
+	}
 
-	Highlighter.prototype.startEraser = function() {
+	startEraser() {
 		drawStyles.initEraseStyle(this);
-	};
+	}
 
-	Highlighter.prototype.saveImage = function() {
+	saveImage() {
 		saveload.save(this);
-	};
+	}
 
-	Highlighter.prototype.loadImage = function() {
+	loadImage() {
 		saveload.load(this);
-	};
+	}
+}
 
-	window.Highlighter = Highlighter;
-
-	return Highlighter;
-
-})(jQuery);
+window.Highlighter = Highlighter;
--- src/js/structure/saveload.js
+++ src/js/structure/saveload.js
@@ -1,7 +1,7 @@
 function save(highlighter){
 	let canvasElement = highlighter.getCanvasElement();
 	let imageData = canvasElement.toDataURL();
-	highlighter.image = imageData;
+	highlighter.imageData = imageData;
 	console.log(imageData);
 	return imageData;
 }
@@ -10,7 +10,7 @@ function load(highlighter){
 	if(highlighter.image){
 		let canvasElement = highlighter.getCanvasElement();
 		let image = new Image();
-		image.src = highlighter.image;
+		image.src = highlighter.imageData;
 		image.onload = function(){
 			canvasElement.getContext('2d').drawImage(image, 0 , 0);
 		};
